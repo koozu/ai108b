c683354e3461bc8b4424ee98b8eecd2a9f8fd78e
koozu <cl4cl4cl40726fff@gmail.com>
2020-05-23 03:23:50 +0800

    modify diffeq.py

--- scientific/diffeq.py
+++ scientific/diffeq.py
@@ -2,4 +2,4 @@ from sympy import Function, dsolve, Eq, Derivative, sin, cos, symbols
 from sympy.abc import C,V,R,t
 V = Function('V')
 sol = dsolve(Derivative(V(t), t)*C + V(t)/R,V(t))
-print('dsolve(Derivative(V(t), t)* + V/R,V(t))=', sol.doit())
+print('dsolve(Derivative(V(t), t)*C + V(t)/R,V(t))=', sol.doit())
