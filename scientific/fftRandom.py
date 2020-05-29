f3cce148b04eea212842c07851f10b453e1bb995
koozu <cl4cl4cl40726fff@gmail.com>
2020-05-29 20:14:40 +0800

    docsify pytorch

---
+++ scientific/fftRandom.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+np.set_printoptions(precision=4, suppress=True)
+freq = random.randint(0,4)
+t = np.arange(0, 10, 0.1)
+ft = np.cos(freq*t*(2*np.pi))
+fi = np.fft.ifft(ft)
+# print('ft=', ft)
+# print('fi=', fi)
+print('freq=', freq)
+
+plt.plot(t[range(int(len(t)/2))],ft[range(int(len(t)/2))],label="$y = 10 sin(f x)$", color="red", linewidth=2)
+plt.plot(t[range(int(len(t)/2))],fi[range(int(len(t)/2))],label="ifft: y$", color="blue", linewidth=2)
+plt.show()
+ 
